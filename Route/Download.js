4eef74abcc381af55777969897b564b1dd1b9959
linux <narendrac024@gmail.com>
2023-04-01 18:34:10 +0530

    ho ja

--- Route/Download.js
+++ Route/Download.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const ytdl = require('ytdl-core');
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+
+
+
+router.get('/convert', (req, res) => {
+  const videoUrl = req.query.url;
+  const command = ffmpeg(videoUrl);
+  
+  command.ffprobe((err, data) => {
+    if (err) {
+      res.status(400).send({ message: 'Invalid video URL' });
+    } else {
+      const availableResolutions = data.streams
+        .filter(stream => stream.codec_type === 'video')
+        .map(stream => stream.height + 'p');
+      res.status(200).send({ resolutions: availableResolutions });
+    }
+  });
+});
+
+module.exports = router;
+
 
 router.get('/', async (req, res) => {
 
@@ -11,5 +34,42 @@ router.get('/', async (req, res) => {
   
   videoStream.pipe(res);
 });
-
+// router.post('/convert', (req, res) => {
+//   const { link } = req.body; // assuming the request body contains the video link
+//   const resolutions = ['144p', '240p', '360p']; // array of resolutions to transcode to
+//   const outputDir = 'output'; // directory to store the transcoded videos
+  
+//   ffmpeg(link) // create FFmpeg input from the video link
+//     .outputOptions('-movflags frag_keyframe+empty_moov') // add output options for smooth streaming
+//     .on('error', (err) => {
+//       console.log('An error occurred: ' + err.message);
+//       res.status(500).send('Error transcoding video');
+//     })
+//     .on('end', () => {
+//       console.log('Transcoding finished');
+//       res.send('Video transcoded successfully');
+//     })
+//     .on('progress', (progress) => {
+//       console.log('Transcoding progress: ' + progress.percent + '%');
+//     })
+//     .output(`${outputDir}/original.mp4`) // transcode the original video to a lower quality
+//     .run();
+  
+//   for (const resolution of resolutions) {
+//     ffmpeg(link) // create FFmpeg input from the video link
+//       .outputOptions('-movflags frag_keyframe+empty_moov') // add output options for smooth streaming
+//       .on('error', (err) => {
+//         console.log('An error occurred: ' + err.message);
+//       })
+//       .on('end', () => {
+//         console.log(`Transcoding to ${resolution} finished`);
+//       })
+//       .on('progress', (progress) => {
+//         console.log(`Transcoding to ${resolution} progress: ` + progress.percent + '%');
+//       })
+//       .output(`${outputDir}/${resolution}.mp4`) // transcode the video to the specified resolution
+//       .size(resolution)
+//       .run();
+//   }
+// });
 module.exports = router;
--- Route/Movies.js
+++ Route/Movies.js
@@ -2,7 +2,7 @@ const express=require("express");
 const router=express.Router();
 const Movie =require("../Models/Movie.js");
 const axios = require('axios');
-
+const cheerio = require('cheerio');
 const API_KEY = '4008ea8497eda5d3e80f32017f7d35bc';
 
 // Create Method
@@ -16,51 +16,41 @@ router.post("/",async (req,res)=>{
 	}
 	 
 });
-router.get('/:name', async (req, res) => {
-	const name = req.params.name;
+
+
+
+router.get('/index', async (req, res) => {
 	try {
-	  const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${name}`);
-	  const results = response.data.results;
-	  const movie = results[0];
-	  const { title, overview, backdrop_path, poster_path, release_date, genre_ids, id } = movie;
-	  const genreResponse = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
-	  const genres = genreResponse.data.genres;
-	  const genreNames = genre_ids.map(id => {
-		const genre = genres.find(genre => genre.id === id);
-		return genre.name;
+	  const response = await axios.get('http://103.87.212.46/Data/Movies/Bollywood/');
+	  const $ = cheerio.load(response.data);
+	  const files = [];
+	  $('a').each((index, element) => {
+		const description = $(element).text();
+		if (description !== '../') {
+		  const trimmed = description.slice(0, -7); // eliminate last 6 characters
+		  files.push(trimmed);
+		}
 	  });
-	  const detailsResponse = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`);
-	  const details = detailsResponse.data;
-	  const trailerKey = details.videos.results[0].key;
-	  const duration = details.runtime;
-	  const responseObj = {
-		title: title,
-		overview: overview,
-		backdrop_path: `https://image.tmdb.org/t/p/original${backdrop_path}`,
-		poster_path: `https://image.tmdb.org/t/p/original${poster_path}`,
-		trailer: `https://www.youtube.com/watch?v=${trailerKey}`,
-		video: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`,
-		release_date: release_date,
-		duration: duration,
-		genre: genreNames.join(', ')
-	  };
-	  const savedMovie = await Movie.create(responseObj);
-	  res.send(savedMovie);
+	  res.status(200).json(files);
 	} catch (error) {
 	  console.error(error);
-	  res.status(500).send('Error fetching movie data');
+	  res.status(500).json({ error: 'Internal server error' });
 	}
   });
-
   
-  //Batch
-  router.post('/batch', async (req, res) => {
-	const { names } = req.body;
+  
+
+
+  router.get('/:name', async (req, res) => {
+	const name = req.params.name;
 	try {
-	  const results = [];
-	  for (const name of names) {
+	  const movie = await Movie.findOne({ title: name });
+	  if (movie) {
+		res.send(movie);
+	  } else {
 		const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${name}`);
-		const movie = response.data.results[0];
+		const results = response.data.results;
+		const movie = results[0];
 		const { title, overview, backdrop_path, poster_path, release_date, genre_ids, id } = movie;
 		const genreResponse = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
 		const genres = genreResponse.data.genres;
@@ -72,7 +62,7 @@ router.get('/:name', async (req, res) => {
 		const details = detailsResponse.data;
 		const trailerKey = details.videos.results[0].key;
 		const duration = details.runtime;
-		const movieObj = {
+		const responseObj = {
 		  title: title,
 		  overview: overview,
 		  backdrop_path: `https://image.tmdb.org/t/p/original${backdrop_path}`,
@@ -81,6 +71,59 @@ router.get('/:name', async (req, res) => {
 		  video: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`,
 		  release_date: release_date,
 		  duration: duration,
+		  genre: genreNames.join(', ')
+		};
+		const savedMovie = await Movie.create(responseObj);
+		res.send(savedMovie);
+	  }
+	} catch (error) {
+	  console.error(error);
+	  res.status(500).send('Error fetching movie data');
+	}
+  });
+  
+
+  
+  //Batch
+  router.post('/batch', async (req, res) => {
+	const { names } = req.body;
+	try {
+	  const results = [];
+	  for (const name of names) {
+		const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${name}`);
+		const movie = response.data.results[0];
+		if (!movie) {
+		  console.log(`No movie found for ${name}`);
+		  continue;
+		}
+		const { title, overview, backdrop_path, poster_path, release_date, genre_ids, id } = movie;
+		if (!title || !overview || !backdrop_path || !poster_path || !release_date || !genre_ids || !id) {
+		  console.log(`Missing data for ${name}`);
+		  continue;
+		}
+		const genreResponse = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
+		const genres = genreResponse.data.genres;
+		const genreNames = genre_ids.map(id => {
+		  const genre = genres.find(genre => genre.id === id);
+		  return genre.name;
+		});
+		const detailsResponse = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&append_to_response=videos`);
+		const details = detailsResponse.data;
+		const trailerKey = details.videos.results.find(video => video.type === 'Trailer')?.key;
+		if (!trailerKey) {
+		  console.log(`Trailer not available for ${name}`);
+		  continue;
+		}
+		const duration = details?.runtime;
+		const movieObj = {
+		  title: title,
+		  overview: overview,
+		  backdrop_path: `https://image.tmdb.org/t/p/original${backdrop_path}`,
+		  poster_path: `https://image.tmdb.org/t/p/original${poster_path}`,
+		  trailer: trailerKey ? `https://www.youtube.com/watch?v=${trailerKey}` : '',
+		  video: `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`,
+		  release_date: release_date,
+		  duration: duration || '',
 		  genre: genreNames.join(', ')
 		};
 		results.push(movieObj);
@@ -92,6 +135,22 @@ router.get('/:name', async (req, res) => {
 	  res.status(500).send('Error fetching movie data');
 	}
   });
+  router.delete('/without-trailer', async (req, res) => {
+	try {
+		const movies = await Movie.find({ trailer: { $exists: true, $ne: '' } });
+		for (const movie of movies) {
+		  if (!movie.trailer) {
+			await Movie.findByIdAndDelete(movie._id);
+		  }
+		}
+		res.send('Deleted movies without trailers');
+	  } catch (error) {
+		console.error(error);
+		res.status(500).send('Error deleting movies without trailers');
+	  }
+	  
+  });
+	
   
 // Update Method
 router.put("/:id",async (req,res)=>{
