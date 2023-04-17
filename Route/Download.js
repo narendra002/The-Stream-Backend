7d62ff005c6cc673520cfc5732338289a135a990
narendra002 <81070103+narendra002@users.noreply.github.com>
2023-04-17 14:06:23 +0530

    Update Download.js

--- Route/Download.js
+++ Route/Download.js
@@ -5,26 +5,20 @@ const ytdl = require('ytdl-core');
 const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
 const http = require('http');
-
-
+const MB = 1024 * 1024;
 router.get('/', async (req, res) => {
-const videoURL = req.query.url;
-  res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-
-  const videoStream = ytdl(videoURL, { format: 'mp4' });
-
-  // Set the Content-Type header based on the video format
-  res.setHeader('Content-Type', 'video/mp4');
-
-  // Send the video stream as the response
-  videoStream.pipe(res);
-
-  // Handle errors
-  videoStream.on('error', (err) => {
-    console.error(err);
-    res.statusCode = 500;
-    res.end('Error fetching video');
-  });
-});
-
+  try {
+    const videoURL = req.query.url;
+    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    const videoStream = ytdl(videoURL, { format: "mp4" });
+    videoStream.on('error', (err) => {
+      console.error(err);
+      res.status(500).send('Error fetching video');
+    });
+    videoStream.pipe(res, { highWaterMark: 1 * MB });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal server error');
+  }
+}); 
 module.exports = router;
