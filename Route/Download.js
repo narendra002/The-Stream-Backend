1d4dacd395faa6db8a0ed616a0d8709a79927b35
linux <narendrac024@gmail.com>
2023-04-17 16:40:15 +0530

    sd

--- Route/Download.js
+++ Route/Download.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const ytdl = require('ytdl-core');
 const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
-const http = require('http');
 const MB = 1024 * 1024;
 const zlib = require('zlib');
 
-router.get('/', async (req, res) => {
+module.exports = async (req, res) => {
   try {
     const videoURL = req.query.url;
-    res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-    res.header('Content-Encoding', 'gzip'); // Set the Content-Encoding header
+    res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+    res.setHeader('Content-Encoding', 'gzip'); // Set the Content-Encoding header
     const videoStream = ytdl(videoURL, { format: "mp4" });
     videoStream.on('error', (err) => {
       console.error(err);
@@ -24,5 +23,6 @@ router.get('/', async (req, res) => {
     console.error(error);
     res.status(500).send('Internal server error');
   }
-}); 
+};
+
 module.exports = router;
