882b7b81a76c70c6b9a8d72b7867ee4eb1430077
narendra002 <81070103+narendra002@users.noreply.github.com>
2023-04-17 14:12:19 +0530

    Update Download.js

--- Route/Download.js
+++ Route/Download.js
@@ -6,16 +6,21 @@ const ffmpeg = require("fluent-ffmpeg");
 const fs = require("fs");
 const http = require('http');
 const MB = 1024 * 1024;
+const zlib = require('zlib');
+
 router.get('/', async (req, res) => {
   try {
     const videoURL = req.query.url;
     res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+    res.header('Content-Encoding', 'gzip'); // Set the Content-Encoding header
     const videoStream = ytdl(videoURL, { format: "mp4" });
     videoStream.on('error', (err) => {
       console.error(err);
       res.status(500).send('Error fetching video');
     });
-    videoStream.pipe(res, { highWaterMark: 1 * MB });
+    videoStream
+      .pipe(zlib.createGzip({ level: zlib.constants.Z_BEST_COMPRESSION })) // Compress the data using gzip
+      .pipe(res, { highWaterMark: 1 * MB });
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal server error');
