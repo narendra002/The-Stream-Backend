6b08dd8daf5bde2ff383026ac8c43f10d575884d
linux <narendrac024@gmail.com>
2023-04-17 17:12:19 +0530

    update

--- Route/Download.js
+++ Route/Download.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const ytdl = require('ytdl-core');
-const fs = require('fs');
 const cors = require('cors');
 
 router.get('/', cors(), (req, res) => {
@@ -9,8 +8,18 @@ router.get('/', cors(), (req, res) => {
   const videoTitle = 'video.mp4';
 
   res.setHeader('Content-Disposition', `attachment; filename="${videoTitle}"`);
+  res.setHeader('Content-Type', 'video/mp4');
 
-  ytdl(videoURL, { format: 'mp4' }).pipe(res);
+  ytdl(videoURL, { format: 'mp4' })
+    .on('error', (err) => {
+      console.error(err);
+      res.status(500).send('Error fetching video');
+    })
+    .on('response', (videoResponse) => {
+      // Set the content length header based on the video size
+      res.setHeader('Content-Length', videoResponse.headers['content-length']);
+    })
+    .pipe(res);
 });
 
 module.exports = router;
